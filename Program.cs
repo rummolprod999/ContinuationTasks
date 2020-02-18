e7d60c6d2a71558561bedd86e0347dc5368ab8f8
rummolprod999 <rummolprod999@gmail.com>
2020-02-18 10:06:49 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ContinuationTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task1 = new Task(()=>{
+                Console.WriteLine($"Id задачи: {Task.CurrentId}");
+            });
+            
+            Task task2 = task1.ContinueWith(Display);
+ 
+            Task task3 = task1.ContinueWith(t =>
+            {
+                Console.WriteLine($"Id задачи: {Task.CurrentId}");
+            });
+ 
+            Task task4 = task2.ContinueWith(t =>
+            {
+                Console.WriteLine($"Id задачи: {Task.CurrentId}");
+            });
+ 
+            task1.Start();
+             
+            Console.ReadLine();
+        }
+        
+        static void Display(Task t)
+        {
+            Console.WriteLine($"Id задачи: {Task.CurrentId}");
+        }
+    }
+}
